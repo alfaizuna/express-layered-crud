324ea915296f4112af8fc7942d6b548893b60cef
alfaizuna <alfaizunaulia@gmail.com>
2024-01-06 23:31:24 +0700

    hello world

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.get("/api", (req, res)=>{
+    res.send("Hello World")
+})
+
 app.listen(PORT, ()=>{
     console.log("express api running in port = " + PORT)
 })
