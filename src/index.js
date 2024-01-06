3353c03d0ed8e5cb9b696cb278f910ca7219b7d5
alfaizuna <alfaizunaulia@gmail.com>
2024-01-06 23:28:17 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log("express api running in port = " + PORT)
+})
