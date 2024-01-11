bc1448ed6b498dc553938886109b6fb1f211927b
alfaizuna <alfaizunaulia@gmail.com>
2024-01-11 23:54:36 +0700

    add products get all API

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-npx prisma init --datasource-provider postgresql 
-npx prisma generate               
-npx prisma db push           
+```npx prisma init --datasource-provider postgresql ```
+
+```npx prisma generate```
+
+```npx prisma db push```           
+
+editor for db prisma
+
+```npx prisma studio```
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const { PrismaClient } = require('@prisma/client');
 
 dotenv.config();
 
+const prisma = new PrismaClient();
 const PORT = process.env.PORT;
 
 app.get("/api", (req, res)=>{
     res.send("Hello World nodemon")
+})
+
+app.get("/products", async (req, res)=>{
+    const products = await prisma.product.findMany();
+    res.send(products);
 })
 
 app.listen(PORT, ()=>{
