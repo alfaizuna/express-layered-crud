b01e964d509e024e931a81bb0a6d0a592e360710
alfaizuna <alfaizunaulia@gmail.com>
2024-01-12 09:58:33 +0700

    create endpoint to add new product to database

--- src/index.js
+++ src/index.js
@@ -8,15 +8,28 @@ dotenv.config();
 const prisma = new PrismaClient();
 const PORT = process.env.PORT;
 
-app.get("/api", (req, res)=>{
+app.get("/api", (req, res) => {
     res.send("Hello World nodemon")
 })
 
-app.get("/products", async (req, res)=>{
+app.get("/products", async (req, res) => {
     const products = await prisma.product.findMany();
     res.send(products);
 })
 
-app.listen(PORT, ()=>{
+app.post("/products", async (req, res) => {
+    const newProduct = req.body;
+    const products = await prisma.product.create({
+        data: {
+            name: newProduct.name,
+            price: newProduct.price,
+            description: newProduct.description,
+            image: newProduct.image
+        }
+    });
+    res.status(201).send("create new product success");
+})
+
+app.listen(PORT, () => {
     console.log("express api running in port = " + PORT)
 })
