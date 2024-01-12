e8d1709404904acf4f7570b03ab2d11b94e641fe
alfaizuna <alfaizunaulia@gmail.com>
2024-01-12 15:48:53 +0700

    apply best practice

--- src/index.js
+++ src/index.js
@@ -1,109 +1,16 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const {PrismaClient} = require('@prisma/client');
-
 dotenv.config();
-
-const prisma = new PrismaClient();
 const PORT = process.env.PORT;
-
+const productController = require("./products/product.controller");
 app.use(express.json());
 
 app.get("/api", (req, res) => {
     res.send("Hello World nodemon")
 })
 
-app.get("/products", async (req, res) => {
-    const products = await prisma.product.findMany();
-    res.send(products);
-})
-
-app.get("/products/:id", async (req, res) => {
-    const productId = req.params.id; //string
-    const product = await prisma.product.findUnique({
-        where: {
-            id: parseInt(productId)
-        }
-    });
-    if (!product) {
-        return res.status(404).send("product not found!");
-    }
-    res.send(product);
-})
-
-app.post("/products", async (req, res) => {
-    const newProduct = req.body;
-    const products = await prisma.product.create({
-        data: {
-            name: newProduct.name,
-            price: newProduct.price,
-            description: newProduct.description,
-            image: newProduct.image
-        }
-    });
-    res.status(201).send({
-        body: products,
-        message: "create new product success"
-    });
-})
-
-app.get("/products/:id", async (req, res) => {
-    const id = req.params.id; //string
-    await prisma.product.delete({
-        where: {
-            id: parseInt(id)
-        }
-    });
-    res.send("product deleted")
-})
-
-app.put("/products/:id", async (req, res) => {
-    const id = req.params.id; //string
-    const productData = req.body;
-
-    if (!(productData.name && productData.description && productData.price && productData.image)) {
-        return res.status(400).send("some field data is missing");
-    }
-
-    const product = await prisma.product.update({
-        where: {
-            id: parseInt(id)
-        },
-        data: {
-            name: productData.name,
-            price: productData.price,
-            description: productData.description,
-            image: productData.image
-        }
-    });
-
-    res.send({
-        body: product,
-        message: "product has been updated"
-    });
-})
-app.patch("/products/:id", async (req, res) => {
-    const id = req.params.id; //string
-    const productData = req.body;
-
-    const product = await prisma.product.update({
-        where: {
-            id: parseInt(id)
-        },
-        data: {
-            name: productData.name,
-            price: productData.price,
-            description: productData.description,
-            image: productData.image
-        }
-    });
-
-    res.send({
-        body: product,
-        message: "product has been updated"
-    });
-})
+app.use("/products", productController);
 
 app.listen(PORT, () => {
     console.log("express api running in port = " + PORT)
---
+++ src/products/product.controller.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+const prisma = require("../database");
+
+router.get("/", async (req, res) => {
+    const products = await prisma.product.findMany();
+    res.send(products);
+})
+
+router.get("/:id", async (req, res) => {
+    const productId = req.params.id; //string
+    const product = await prisma.product.findUnique({
+        where: {
+            id: parseInt(productId)
+        }
+    });
+    if (!product) {
+        return res.status(404).send("product not found!");
+    }
+    res.send(product);
+})
+
+router.post("/", async (req, res) => {
+    const newProduct = req.body;
+    const products = await prisma.product.create({
+        data: {
+            name: newProduct.name,
+            price: newProduct.price,
+            description: newProduct.description,
+            image: newProduct.image
+        }
+    });
+    res.status(201).send({
+        body: products,
+        message: "create new product success"
+    });
+})
+
+router.get("/:id", async (req, res) => {
+    const id = req.params.id; //string
+    await prisma.product.delete({
+        where: {
+            id: parseInt(id)
+        }
+    });
+    res.send("product deleted")
+})
+
+router.put("/:id", async (req, res) => {
+    const id = req.params.id; //string
+    const productData = req.body;
+
+    if (!(productData.name && productData.description && productData.price && productData.image)) {
+        return res.status(400).send("some field data is missing");
+    }
+
+    const product = await prisma.product.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: {
+            name: productData.name,
+            price: productData.price,
+            description: productData.description,
+            image: productData.image
+        }
+    });
+
+    res.send({
+        body: product,
+        message: "product has been updated"
+    });
+})
+router.patch("/:id", async (req, res) => {
+    const id = req.params.id; //string
+    const productData = req.body;
+
+    const product = await prisma.product.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: {
+            name: productData.name,
+            price: productData.price,
+            description: productData.description,
+            image: productData.image
+        }
+    });
+
+    res.send({
+        body: product,
+        message: "product has been updated"
+    });
+})
+
+module.exports = router;
